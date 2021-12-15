f8f5de26a692c4513f2f76e911794a5b53a9304a
DUCLOS Maxime <mduclos@sodifrance.fr>
2021-12-15 22:29:50 +0100

    Active le CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {AppModule} from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     const host = '0.0.0.0';
     await app.listen(parseInt(process.env.PORT, 10) || 3000, host);
 }
